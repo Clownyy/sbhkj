627c4ddc4f8b20a4defcd3bd35e404ddda34fa5b
unknown <bafaqih555@gmail.com>
2020-02-05 14:20:08 +0700

    first

--- application/views/login.php
+++ application/views/login.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Admin Evote | Log in</title>
+  <title>Admin LSPResto | Log in</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -30,11 +30,11 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>MyPOS</b> by NwGrd</a>
+    <a href="../../index2.html"><b>LSPResto</b> by NwGrd</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
-    <p class="login-box-msg">Sign in to manage POS</p>
+    <p class="login-box-msg">Sign in to manage Resto</p>
 
     <form action="<?=base_url('auth/process')?>" method="post">
       <div class="form-group has-feedback">
